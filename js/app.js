8a88ef3af524abcc59f98b605c1c35db637dc6e4
Akash Chavan <meakashchavan@gmail.com>
2017-01-24 22:57:33 +0530

    success retrieving data from wikipedia api.

---
+++ js/app.js
@@ -0,0 +1,37 @@
+function loadData(){
+  var body = $('body');
+  var $wikiElem = $('#wikipedia-links');
+
+  // clear out old data before new request
+  $wikiElem.text("");
+
+  var search_input = $("#search-input").val();
+
+  console.log(search_input);
+
+  var wikipedia_url = "http://en.wikipedia.org/w/api.php";
+  $.ajax({
+      url: wikipedia_url,
+      type: 'GET',
+      dataType: 'jsonp',
+      data: {action: 'query', list: 'search', srsearch: $("input[name=Wikipedia]").val(), format: 'json' }
+    })
+    .done(function(response) {
+      //console.log(response);
+      var art = response.query.search;
+      console.log(art);
+      art.forEach(function(val){
+        //console.log(val.snippet);
+        $wikiElem.append('<li class = "article">'+
+      val.title+'<p>'+val.snippet+'</p>');
+      });
+    })
+    .fail(function() {
+      console.log("error");
+    })
+    .always(function() {
+      console.log("complete");
+    });
+    return false;
+}
+$('#form-container').submit(loadData);
